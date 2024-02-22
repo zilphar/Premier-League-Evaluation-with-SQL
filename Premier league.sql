012bbe2dfeadd0b673a2ee3ea171de3a39df265e
zilphar <116642579+zilphar@users.noreply.github.com>
2024-02-23 02:13:05 +0300

    Add files via upload

--- Premier league.sql
+++ Premier league.sql
@@ -25,12 +25,13 @@ SELECT
 	DISTINCT Referee
 FROM [season-0910]; 
 
-
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- 1. The number of matches Manchester United won either home and away (27 matches)
--- The CASE statement checks each row to see if it meets certain conditions. 
--- If the condition is true, it returns 1; otherwise, it returns 0. 
--- The SUM function then adds up all these 1s and 0s, effectively counting the number of rows that meet the condition. 
--- Here, it's counting the number of matches where Manchester United won, either as the home team (HomeTeam = 'Manchester United' and FTR = 'H') or as the away team (AwayTeam = 'Manchester United' and FTR = 'A'). The result is assigned an alias total_wins.
+/*The CASE statement checks each row to see if it meets certain conditions. 
+	If the condition is true, it returns 1; otherwise, it returns 0. 
+	The SUM function then adds up all these 1s and 0s, effectively counting the number of rows that meet the condition. 
+	Here, it's counting the number of matches where Manchester United won, either as the home team (HomeTeam = 'Manchester United' and FTR = 'H') or 
+	as the away team (AwayTeam = 'Manchester United' and FTR = 'A'). The result is assigned an alias total_wins.*/
 
 SELECT
 	SUM(CASE WHEN(HomeTeam = 'Man United' AND FTR = 'H') OR (AwayTeam = 'Man United' AND FTR = 'A') THEN 1 ELSE 0 END) AS total_ManU_wins
@@ -39,8 +40,8 @@ WHERE (HomeTeam = 'Man United' OR AwayTeam = 'Man United');
 
 
 --2. Number of matches Manchester won at home and away (16 homewins, and 11 away wins)
--- In this case, the CASE statement counts the number of matches where Manchester United won as the home team (FTR = 'H'). The result of this calculation is aliased as Home_wins. 
--- It then counts the number of matches where Manchester United won as the away team (FTR = 'A'). The result of this calculation is aliased as away_wins. 
+/* In this case, the CASE statement counts the number of matches where Manchester United won as the home team (FTR = 'H'). The result of this calculation is aliased as Home_wins. 
+It then counts the number of matches where Manchester United won as the away team (FTR = 'A'). The result of this calculation is aliased as away_wins. */
 
 SELECT 
 	SUM(CASE WHEN HomeTeam = 'Man United' AND FTR = 'H' THEN 1 ELSE 0 END) AS Home_wins,
@@ -50,10 +51,10 @@ WHERE (HomeTeam = 'Man United' OR AwayTeam = 'Man United');
 
 
 -- 3. Number of goals manchester scored during the season 
---The first SUM function calculates the total number of goals scored by Manchester United in home matches (HomeTeam = 'Manchester United'). 
---It uses the FTHG (Full-Time Home Goals) column to count the goals scored.
---The second SUM function calculates the total number of goals scored by Manchester United in away matches (AwayTeam = 'Manchester United'). 
---It uses the FTAG (Full-Time Away Goals) column to count the goals scored. 
+/*The first SUM function calculates the total number of goals scored by Manchester United in home matches (HomeTeam = 'Manchester United'). 
+	It uses the FTHG (Full-Time Home Goals) column to count the goals scored.
+	The second SUM function calculates the total number of goals scored by Manchester United in away matches (AwayTeam = 'Manchester United'). 
+	It uses the FTAG (Full-Time Away Goals) column to count the goals scored. */
 	
 	-- total home and  away goals in season 0910 (52 home goals, and 34 away goals)
 SELECT 
@@ -111,6 +112,46 @@ SELECT
 FROM [season-0910]
 GROUP BY Referee; 
 
+-- 7. Teams that had the highest home and away goals
+SELECT HomeTeam,
+	SUM(FTHG) AS h_goals    -- Chelsea is leading with 68 home goals with Man United beeing the second with 52 goals
+FROM [season-0910]
+GROUP BY HomeTeam
+ORDER BY h_goals DESC;
+
+SELECT AwayTeam,
+	SUM(FTAG) AS a_goals    -- Arsenal and Chelsea lead with 35 away goals each 
+FROM [season-0910]
+GROUP BY AwayTeam
+ORDER BY a_goals DESC;
+
+-- 8. Teams with the most matches won in both home and away 
+SELECT HomeTeam,
+	COUNT(FTR) AS home_win    -- Chelsea had the most matches won at 17 followed by Man United at 16 won home matches. 
+FROM [season-0910]
+WHERE FTR = 'H'
+GROUP BY HomeTeam
+ORDER BY home_win DESC; 
+
+SELECT AwayTeam, 
+	COUNT(FTR) AS away_win   -- Man United had the most won away matches with 11 followed by Chelsea at 10 won away matches. 
+FROM [season-0910]
+WHERE FTR = 'A'
+GROUP BY AwayTeam
+ORDER BY away_win DESC; 
+
+-- 9 Numeber of draw matches in the Season0910
+SELECT 
+	COUNT(FTR) AS Draws  -- 96 total draws 
+FROM [season-0910]
+WHERE FTR = 'D'; 
+
+-- out of the draws from the season which ones are from when Man United was the Home team
+
+SELECT 
+	COUNT(FTR) AS ManU_Draws
+FROM [season-0910]
+WHERE (HomeTeam = 'Man United' OR AwayTeam = 'Man United') AND FTR = 'D'; 
 
 
 
