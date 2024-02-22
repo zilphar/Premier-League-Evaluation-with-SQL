eaa77983ad8f036e64c820912a026a12ddea99e3
zilphar <116642579+zilphar@users.noreply.github.com>
2024-02-23 02:18:30 +0300

    Update Premier league.sql

--- Premier league.sql
+++ Premier league.sql
@@ -27,11 +27,10 @@ FROM [season-0910];
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- 1. The number of matches Manchester United won either home and away (27 matches)
-/*The CASE statement checks each row to see if it meets certain conditions. 
-	If the condition is true, it returns 1; otherwise, it returns 0. 
-	The SUM function then adds up all these 1s and 0s, effectively counting the number of rows that meet the condition. 
-	Here, it's counting the number of matches where Manchester United won, either as the home team (HomeTeam = 'Manchester United' and FTR = 'H') or 
-	as the away team (AwayTeam = 'Manchester United' and FTR = 'A'). The result is assigned an alias total_wins.*/
+--The CASE statement checks each row to see if it meets certain conditions. 
+	--If the condition is true, it returns 1; otherwise, it returns 0. 
+	--The SUM function then adds up all these 1s and 0s, effectively counting the number of rows that meet the condition. 
+	--Here, it's counting the number of matches where Manchester United won, either as the home team (HomeTeam = 'Manchester United' and FTR = 'H') or as the away team (AwayTeam = 'Manchester United' and FTR = 'A'). The result is assigned an alias total_wins.
 
 SELECT
 	SUM(CASE WHEN(HomeTeam = 'Man United' AND FTR = 'H') OR (AwayTeam = 'Man United' AND FTR = 'A') THEN 1 ELSE 0 END) AS total_ManU_wins
@@ -40,8 +39,8 @@ WHERE (HomeTeam = 'Man United' OR AwayTeam = 'Man United');
 
 
 --2. Number of matches Manchester won at home and away (16 homewins, and 11 away wins)
-/* In this case, the CASE statement counts the number of matches where Manchester United won as the home team (FTR = 'H'). The result of this calculation is aliased as Home_wins. 
-It then counts the number of matches where Manchester United won as the away team (FTR = 'A'). The result of this calculation is aliased as away_wins. */
+--In this case, the CASE statement counts the number of matches where Manchester United won as the home team (FTR = 'H'). The result of this calculation is aliased as Home_wins. 
+	--It then counts the number of matches where Manchester United won as the away team (FTR = 'A'). The result of this calculation is aliased as away_wins. 
 
 SELECT 
 	SUM(CASE WHEN HomeTeam = 'Man United' AND FTR = 'H' THEN 1 ELSE 0 END) AS Home_wins,
@@ -51,10 +50,10 @@ WHERE (HomeTeam = 'Man United' OR AwayTeam = 'Man United');
 
 
 -- 3. Number of goals manchester scored during the season 
-/*The first SUM function calculates the total number of goals scored by Manchester United in home matches (HomeTeam = 'Manchester United'). 
-	It uses the FTHG (Full-Time Home Goals) column to count the goals scored.
-	The second SUM function calculates the total number of goals scored by Manchester United in away matches (AwayTeam = 'Manchester United'). 
-	It uses the FTAG (Full-Time Away Goals) column to count the goals scored. */
+--The first SUM function calculates the total number of goals scored by Manchester United in home matches (HomeTeam = 'Manchester United'). 
+	--It uses the FTHG (Full-Time Home Goals) column to count the goals scored.
+	--The second SUM function calculates the total number of goals scored by Manchester United in away matches (AwayTeam = 'Manchester United'). 
+	--It uses the FTAG (Full-Time Away Goals) column to count the goals scored. 
 	
 	-- total home and  away goals in season 0910 (52 home goals, and 34 away goals)
 SELECT 
@@ -140,7 +139,7 @@ WHERE FTR = 'A'
 GROUP BY AwayTeam
 ORDER BY away_win DESC; 
 
--- 9 Numeber of draw matches in the Season0910
+-- 9 Number of draw matches in the Season0910
 SELECT 
 	COUNT(FTR) AS Draws  -- 96 total draws 
 FROM [season-0910]
