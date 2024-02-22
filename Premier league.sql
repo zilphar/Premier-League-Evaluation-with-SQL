20f1b7d58ea80ad2f9d7a56802f11947a84b2742
zilphar <116642579+zilphar@users.noreply.github.com>
2024-02-22 23:07:25 +0300

    Update Premier league.sql

--- Premier league.sql
+++ Premier league.sql
@@ -39,8 +39,8 @@ WHERE (HomeTeam = 'Man United' OR AwayTeam = 'Man United');
 
 
 --2. Number of matches Manchester won at home and away (16 homewins, and 11 away wins)
-/* In this case, the CASE statement counts the number of matches where Manchester United won as the home team (FTR = 'H'). The result of this calculation is aliased as Home_wins. 
-It then counts the number of matches where Manchester United won as the away team (FTR = 'A'). The result of this calculation is aliased as away_wins. */
+-- In this case, the CASE statement counts the number of matches where Manchester United won as the home team (FTR = 'H'). The result of this calculation is aliased as Home_wins. 
+-- It then counts the number of matches where Manchester United won as the away team (FTR = 'A'). The result of this calculation is aliased as away_wins. 
 
 SELECT 
 	SUM(CASE WHEN HomeTeam = 'Man United' AND FTR = 'H' THEN 1 ELSE 0 END) AS Home_wins,
@@ -50,10 +50,10 @@ WHERE (HomeTeam = 'Man United' OR AwayTeam = 'Man United');
 
 
 -- 3. Number of goals manchester scored during the season 
-/*The first SUM function calculates the total number of goals scored by Manchester United in home matches (HomeTeam = 'Manchester United'). 
-	It uses the FTHG (Full-Time Home Goals) column to count the goals scored.
-	The second SUM function calculates the total number of goals scored by Manchester United in away matches (AwayTeam = 'Manchester United'). 
-	It uses the FTAG (Full-Time Away Goals) column to count the goals scored. */
+--The first SUM function calculates the total number of goals scored by Manchester United in home matches (HomeTeam = 'Manchester United'). 
+--It uses the FTHG (Full-Time Home Goals) column to count the goals scored.
+--The second SUM function calculates the total number of goals scored by Manchester United in away matches (AwayTeam = 'Manchester United'). 
+--It uses the FTAG (Full-Time Away Goals) column to count the goals scored. 
 	
 	-- total home and  away goals in season 0910 (52 home goals, and 34 away goals)
 SELECT 
